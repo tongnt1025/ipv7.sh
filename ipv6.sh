e67627baa494e2d07f8448cdcdd2377ace1159a7
Nguyễn Thanh Tòng <93186273+tongnt1025@users.noreply.github.com>
2024-07-11 10:32:25 +0700

    Update ipv6.sh

--- ipv6.sh
+++ ipv6.sh
@@ -1,79 +1,64 @@
 #!/bin/bash
-# define some constants. Those are your WAN IP which need to be whitelisted.
-fpt_ip="103.121.88.0/24"
-vnpt_ip="103.48.81.0/24"
-ip_vphn="14.162.145.194"
-ip_vphcm="14.241.230.218"
-# delete all configuration and set default policy for each chain.
-iptables -F
-iptables -X
-iptables -P INPUT ACCEPT
-iptables -P OUTPUT ACCEPT
-iptables -P FORWARD ACCEPT
-iptables -N ZIMBRA-FIREWALL
-iptables -A INPUT -j ZIMBRA-FIREWALL
-iptables -A FORWARD -j ZIMBRA-FIREWALL
-# Stop some attacks
-iptables -A ZIMBRA-FIREWALL -p tcp --tcp-flags ALL NONE -j DROP
-iptables -A ZIMBRA-FIREWALL -p tcp ! --syn -m state --state NEW -j DROP
-iptables -A ZIMBRA-FIREWALL -p tcp --tcp-flags ALL ALL -j DROP
-# Accept save connection and icmp
-iptables -A ZIMBRA-FIREWALL -i lo -j ACCEPT
-iptables -A ZIMBRA-FIREWALL -p icmp --icmp-type any -j ACCEPT
-iptables -A ZIMBRA-FIREWALL -m state --state ESTABLISHED,RELATED -j ACCEPT
-# Drop invalid packets immediately
-#iptables -A INPUT   -m state --state INVALID -j DROP
-#iptables -A FORWARD -m state --state INVALID -j DROP
-#iptables -A OUTPUT  -m state --state INVALID -j DROP
-iptables -A ZIMBRA-FIREWALL -m state --state INVALID -j DROP
-# Drop bogus TCP packets
-#iptables -A INPUT -p tcp -m tcp --tcp-flags SYN,FIN SYN,FIN -j DROP
-iptables -A ZIMBRA-FIREWALL -p tcp -m tcp --tcp-flags SYN,FIN SYN,FIN -j DROP
-#iptables -A INPUT -p tcp -m tcp --tcp-flags SYN,RST SYN,RST -j DROP
-iptables -A ZIMBRA-FIREWALL -p tcp -m tcp --tcp-flags SYN,RST SYN,RST -j DROP
-#iptables -A INPUT -p tcp -m tcp --tcp-flags RST RST -m limit --limit 2/second --limit-burst 2 -j ACCEPT
-iptables -A ZIMBRA-FIREWALL -p tcp -m tcp --tcp-flags RST RST -m limit --limit 2/second --limit-burst 2 -j ACCEPT
-# enable this rule if have has layer remote 2/3 devices
-iptables -A ZIMBRA-FIREWALL -p tcp --tcp-flags FIN,SYN,RST,ACK RST,ACK -j ACCEPT
-# enable ssh and snmp
-#iptables -A ZIMBRA-FIREWALL -m state -state NEW -m tcp -p tcp -dport 22 -j ACCEPT -s $vnpt_ip
-#iptables -A ZIMBRA-FIREWALL -m state -state NEW -m tcp -p tcp -dport 22 -j ACCEPT -s $fpt_ip
-#iptables -A ZIMBRA-FIREWALL -m state -state NEW -m udp -p udp -dport 161 -j ACCEPT -s $vnpt_ip
-#iptables -A ZIMBRA-FIREWALL -m state -state NEW -m udp -p udp -dport 161 -j ACCEPT -s $fpt_ip
-iptables -A ZIMBRA-FIREWALL -j ACCEPT -s $fpt_ip
-iptables -A ZIMBRA-FIREWALL -j ACCEPT -s $vnpt_ip
-iptables -A ZIMBRA-FIREWALL -j ACCEPT -s $ip_vphn
-iptables -A ZIMBRA-FIREWALL -j ACCEPT -s $ip_vphcm
-# enable zimbra ports
-iptables -A ZIMBRA-FIREWALL -m state --state NEW -m tcp -p tcp --dport 25 -j ACCEPT
-iptables -A ZIMBRA-FIREWALL -m state --state NEW -m tcp -p tcp --dport 80 -j ACCEPT
-iptables -A ZIMBRA-FIREWALL -m state --state NEW -m tcp -p tcp --dport 110 -j ACCEPT
-iptables -A ZIMBRA-FIREWALL -m state --state NEW -m tcp -p tcp --dport 143 -j ACCEPT
-iptables -A ZIMBRA-FIREWALL -m state --state NEW -m tcp -p tcp --dport 443 -j ACCEPT
-iptables -A ZIMBRA-FIREWALL -m state --state NEW -m tcp -p tcp --dport 465 -j ACCEPT
-iptables -A ZIMBRA-FIREWALL -m state --state NEW -m tcp -p tcp --dport 587 -j ACCEPT
-iptables -A ZIMBRA-FIREWALL -m state --state NEW -m tcp -p tcp --dport 993 -j ACCEPT
-iptables -A ZIMBRA-FIREWALL -m state --state NEW -m tcp -p tcp --dport 995 -j ACCEPT
-iptables -A ZIMBRA-FIREWALL -m state --state NEW -m tcp -p tcp -dport 7071 -j ACCEPT
-#iptables -A ZIMBRA-FIREWALL -m state --state NEW -m tcp -p tcp -dport 9071 -j ACCEPT
-# log and reject everything else
-#iptables -A ZIMBRA-FIREWALL -j LOG -m limit -limit 10/m -log-prefix “DROP ON INPUT: ” -log-tcp-options -log-ip-options -log-level INFO
-iptables -A ZIMBRA-FIREWALL -j DROP
-# save configuraton and restart iptables
-#/etc/rc.d/init.d/iptables save
-#/etc/rc.d/init.d/iptables 
-## save on ubuntu: apt-get install iptables-persistent
-## save on cetnos 7: 
-cat > /etc/systemd/system/iptables-restore.service <<EOF
-[Unit]
-Description=IPv4 iptables firewall rules
-After=network.target
-
-[Service]
-Type=oneshot
-ExecStart=/usr/sbin/iptables-restore /etc/iptables/rules.v4
-RemainAfterExit=true
-
-[Install]
-WantedBy=multi-user.target
+YUM=$(which yum)
+#####
+if [ "$YUM" ]; then
+echo > /etc/sysctl.conf
+##
+tee -a /etc/sysctl.conf <<EOF
+net.ipv6.conf.default.disable_ipv6 = 0
+net.ipv6.conf.all.disable_ipv6 = 0
 EOF
+##
+sysctl -p
+IPC=$(curl -4 -s icanhazip.com | cut -d"." -f3)
+IPD=$(curl -4 -s icanhazip.com | cut -d"." -f4)
+##
+if [ $IPC == 4 ]
+then
+   tee -a /etc/sysconfig/network-scripts/ifcfg-eth0 <<-EOF
+	IPV6INIT=yes
+	IPV6_AUTOCONF=no
+	IPV6_DEFROUTE=yes
+	IPV6_FAILURE_FATAL=no
+	IPV6_ADDR_GEN_MODE=stable-privacy
+	IPV6ADDR=2403:6a40:0:40::$IPD:0000/64
+	IPV6_DEFAULTGW=2403:6a40:0:40::1
+	EOF
+elif [ $IPC == 5 ]
+then
+   tee -a /etc/sysconfig/network-scripts/ifcfg-eth0 <<-EOF
+	IPV6INIT=yes
+	IPV6_AUTOCONF=no
+	IPV6_DEFROUTE=yes
+	IPV6_FAILURE_FATAL=no
+	IPV6_ADDR_GEN_MODE=stable-privacy
+	IPV6ADDR=2403:6a40:0:41::$IPD:0000/64
+	IPV6_DEFAULTGW=2403:6a40:0:41::1
+	EOF
+elif [ $IPC == 244 ]
+then
+   tee -a /etc/sysconfig/network-scripts/ifcfg-eth0 <<-EOF
+	IPV6INIT=yes
+	IPV6_AUTOCONF=no
+	IPV6_DEFROUTE=yes
+	IPV6_FAILURE_FATAL=no
+	IPV6_ADDR_GEN_MODE=stable-privacy
+	IPV6ADDR=2403:6a40:2000:244::$IPD:0000/64
+	IPV6_DEFAULTGW=2403:6a40:2000:244::1
+	EOF
+else
+	tee -a /etc/sysconfig/network-scripts/ifcfg-eth0 <<-EOF
+	IPV6INIT=yes
+	IPV6_AUTOCONF=no
+	IPV6_DEFROUTE=yes
+	IPV6_FAILURE_FATAL=no
+	IPV6_ADDR_GEN_MODE=stable-privacy
+	IPV6ADDR=2403:6a40:0:$IPC::$IPD:0000/64
+	IPV6_DEFAULTGW=2403:6a40:0:$IPC::1
+	EOF
+fi
+
+service NetworkManager restart
+
+fi
+echo 'Da tao IPV6 thanh cong!'
